b270b102c7842c5ae7a4f765894cd1e1ef936afc
AziziRahim <azizirahim67@gmail.com>
2024-11-15 15:57:26 +0700

    uts

---
+++ Alumni/routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\AlumniController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/alumnis',[AlumniController::class, 'index']);
+Route::post('/alumnis',[AlumniController::class, 'store']);
+Route::put('/alumnis/{id}',[AlumniController::class, 'update']);
+Route::delete('/alumnis/{id}',[AlumniController::class,'destroy']);
+Route::get('/alumnis/{id}',[AlumniController::class, 'show'] );
+Route::get('/alumnis/seacrh/{name}',[AlumniController::class, 'search'] );
+Route::get('/alumnis/status/fresh-graduate',[AlumniController::class, 'freshGraduate'] );
+Route::get('/alumnis/status/employed',[AlumniController::class, 'employed'] );
+Route::get('/alumnis/status/unemployed',[AlumniController::class, 'unemployed'] );
+
