66282db12315eb4fbef76ee788918f0da93ac080
AziziRahim <azizirahim67@gmail.com>
2025-01-10 16:46:12 +0700

    pertemuan-13

---
+++ Pertemuan-13/task/app.js
@@ -0,0 +1,18 @@
+// import express dan routing
+const express = require("express");
+const router = require("./routes/api");
+
+
+// Membuat object express
+const app = express();
+
+// Menggunakan middleware
+app.use(express.json());
+app.use(express.urlencoded());
+
+// Menggunakan routing (router)
+app.use(router);
+
+// Mendefinisikan port.
+app.listen(3000);
+console.log("Server berjalan di port 3000");  // untuk mengetahui
