9504686b6335ce17ad5e4550b7bab814fd1feb9b
AziziRahim <128167514+AziziRahim@users.noreply.github.com>
2025-01-17 15:49:43 +0700

    Delete Pertemuan-11 directory

--- Pertemuan-11/task/data/students.js
+++
@@ -1,7 +0,0 @@
-// TODO 1: Buat data students
-// code here
-const students = ["Mikel", "Hannah", "Jonnas"]
-
-// TODO 2: export data students
-// code here
-module.exports = students; // export data students
